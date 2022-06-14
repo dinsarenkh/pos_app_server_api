102bea326ae5deae8bafd765d87a1c7c824a8495
dinsaren <dinsarenkh@gmail.com>
2022-06-14 18:22:14 +0700

    update user register

--- src/main/java/com/dinsaren/bbuappserver/payload/req/RegisterReq.java
+++ src/main/java/com/dinsaren/bbuappserver/payload/req/RegisterReq.java
@@ -1,49 +1,16 @@
 package com.dinsaren.bbuappserver.payload.req;
 
+import lombok.Data;
+
+@Data
 public class RegisterReq {
     private String username;
     private String email;
     private String phone;
-//    private Set<String> role;
     private String password;
-  
-    public String getUsername() {
-        return username;
-    }
- 
-    public void setUsername(String username) {
-        this.username = username;
-    }
- 
-    public String getEmail() {
-        return email;
-    }
- 
-    public void setEmail(String email) {
-        this.email = email;
-    }
- 
-    public String getPassword() {
-        return password;
-    }
- 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    private String profile;
+    private String nationalId;
+    private String nationalImage;
+    private String address;
 
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-//    public Set<String> getRole() {
-//      return this.role;
-//    }
-//
-//    public void setRole(Set<String> role) {
-//      this.role = role;
-//    }
 }
